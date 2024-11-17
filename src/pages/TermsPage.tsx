5e9a3d16094403d16522e21825f5366488a9fc4f
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2024-11-17 20:06:34 +0200

    add terms page

---
+++ src/pages/TermsPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TermsPage: React.FC = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Privacy Policy</h1>
+            <h2>1. The standard Lorem Ipsum passage</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+            <h2>2. Another standard Lorem Ipsum passage</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+            <button>Back</button>
+        </div>
+    );
+};
+
+export default TermsPage;
